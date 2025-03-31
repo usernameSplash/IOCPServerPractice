45c96f9f9ddaf6508610c30b64c3fafe5f38b931
Kyeongmin Kim <kkfenix3085@naver.com>
2025-03-31 20:54:05 +0900

    Move Static Variable Declare

--- Utils/LockFreeQueue/LockFreeQueue/PerformanceTest.cpp
+++ Utils/LockFreeQueue/LockFreeQueue/PerformanceTest.cpp
@@ -1,5 +1,18 @@
 #include "PerformanceTest.h"
 
+static LockFreeQueue<int> s_lockFreeQueue;
+static std::queue<int> s_lockQueue;
+static LARGE_INTEGER s_freq;
+static SRWLOCK s_queueLock;
+
+static double s_LockFreeTotalDuration[THREAD_NUM] = { 0, };
+static double s_LockFreeEnqueueDuration[THREAD_NUM] = { 0, };
+static double s_LockFreeDequeueDuration[THREAD_NUM] = { 0, };
+
+static double s_LockTotalDuration[THREAD_NUM] = { 0, };
+static double s_LockEnqueueDuration[THREAD_NUM] = { 0, };
+static double s_LockDequeueDuration[THREAD_NUM] = { 0, };
+
 unsigned int WINAPI LockFreeQueueTest(void* arg)
 {
 	LARGE_INTEGER total_start;
