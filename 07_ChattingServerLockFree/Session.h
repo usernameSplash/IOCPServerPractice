3f1d84202e5db4d8e82d0334c91c3b02b570df77
Kyeongmin Kim <kkfenix3085@naver.com>
2026-09-14 16:28:53 +0900

    Add Volatile Keyword to Session Active Flag For Code Sementic

--- 07_ChattingServerLockFree/Session.h
+++ 07_ChattingServerLockFree/Session.h
@@ -44,7 +44,7 @@ public:
 	void Terminate(void);
 
 private:
-	bool _isActive = false;
+	volatile bool _isActive = false;
 
 	SessionID _sessionId = ULLONG_MAX;
 	SOCKET _clientSocket = INVALID_SOCKET;
